0a4b5679c46b8e29745fda736e7665d4e808fb62
JossJL <joss.lazenby@gmail.com>
2026-07-08 16:47:59 +0100

    Updated ReadMe

--- extract.py
+++ extract.py
@@ -31,19 +31,32 @@ logger=logging.getLogger()
 logger.info('Logging Initialised')
 
 while attempt < max_retry:
+    attempt+=1
+    logger.info(f'Attempt {attempt}')
     response = requests.get(url)
     status_code = response.status_code
     if 200 <= status_code < 300:
         data = response.json()
-        with open(filename,'w') as file:
-            json.dump(data,file)
-        print('Data extracted successfully!')
-        break
+        if len(data) > 0:
+            try:
+                with open(filename,'w') as file:
+                    json.dump(data,file)
+                print('Data extracted successfully!')
+                logger.info(f'File {filename} successfully saved')
+                break
+            except Exception as e:
+                logger.error(f'An error occurred: {e}')
+        else:
+            print('No data returned!')
+            logger.error('No data returned')
+            break
+
     elif status_code <= 100 or status_code >= 500:
-        attempt+=1
         print(f'Error - retrying in 10s attempt {attempt}')
+        logger.warning(f'Error - retrying in 10s attempt {attempt}')
         time.sleep(delay)
     else:
         print(f'API Error Code {status_code} - cancelled')
+        logger.error(f'Bad response from API. Error code {status_code}')
         break
         
